b6f03221c8c336406264949d771412a0e04e50af
SHIVAM SINGH <ssultanpura9@GMail.com>
2024-10-18 17:59:47 +0530

    added login functionality

--- src/App.js
+++ src/App.js
@@ -24,54 +24,64 @@ import UserDetails from "./components/UserDetails";
 import DashBoard from "./components/DashBoard";
 import Login from "./components/Login";
 
-
 function App() {
   const [smallSideBarActivated, setSmallSideBarActivated] = useState(false);
   const { width, height } = useWindowSize();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   console.log(width, height);
+  const handleLogin=()=>{
+    setIsLoggedIn(true)
+  }
   return (
     <div className="bg-[#222736] w-[100vw] h-[100vh]">
-      {/* <Navbar
-        handleSmallSideBar={() => {
-          setSmallSideBarActivated(!smallSideBarActivated);
-        }}
-        smallSideBarActivated={smallSideBarActivated}
-      ></Navbar>
-      <div className="flex h-[calc(100vh-70px)]">
-      
-        {width >= 992 && !smallSideBarActivated ? (
-          <BigScreenSideBar />
-        ) : width >= 992 && smallSideBarActivated ? (
-          <IconScreenSideBar />
-        ) : (
-          !smallSideBarActivated && <BigScreenSideBar />
-        )}
+      {isLoggedIn ? (
+        <>
+          {" "}
+          <Navbar
+            handleSmallSideBar={() => {
+              setSmallSideBarActivated(!smallSideBarActivated);
+            }}
+            smallSideBarActivated={smallSideBarActivated}
+          ></Navbar>
+          <div className="flex h-[calc(100vh-70px)]">
+            {width >= 992 && !smallSideBarActivated ? (
+              <BigScreenSideBar />
+            ) : width >= 992 && smallSideBarActivated ? (
+              <IconScreenSideBar />
+            ) : (
+              !smallSideBarActivated && <BigScreenSideBar />
+            )}
 
+            <Routes>
+              <Route path="/addMovies" element={<AddMovies />}></Route>
+              <Route path="/addWebShows" element={<AddWebSeries />}></Route>
+              <Route path="/addLayout" element={<AddLayout />}></Route>
+              <Route path="/addSlider" element={<AddSlider />}></Route>
+              <Route path="/allMovies" element={<AllMovies />}></Route>
+              <Route path="/allMovies/:edit" element={<EditMovies />}></Route>
+              <Route path="/allLayouts" element={<AllLAyout />}></Route>
+              <Route path="/allLayout/:edit" element={<EditLayout />}></Route>
+              <Route path="/allSliders" element={<AllSliders />}></Route>
+              <Route path="/genreslist" element={<AllGenreList />}></Route>
+              <Route
+                path="/LanguageList"
+                element={<LanguageList />}
+              ></Route>{" "}
+              <Route
+                path="/QuestionUploader"
+                element={<EdducationQuestionGenerator />}
+              ></Route>{" "}
+              <Route path="allUsers" element={<AllUsers />}></Route>
+              <Route path="/userDetails/:uid" element={<UserDetails />}></Route>
+              <Route path="*" element={<DashBoard />}></Route>
+            </Routes>
+          </div>
+        </>
+      ) : (
         <Routes>
-          <Route path="/addMovies" element={<AddMovies />}></Route>
-          <Route path="/addWebShows" element={<AddWebSeries />}></Route>
-          <Route path="/addLayout" element={<AddLayout />}></Route>
-          <Route path="/addSlider" element={<AddSlider />}></Route>
-          <Route path="/allMovies" element={<AllMovies />}></Route>
-          <Route path="/allMovies/:edit" element={<EditMovies />}></Route>
-          <Route path="/allLayouts" element={<AllLAyout />}></Route>
-          <Route path="/allLayout/:edit" element={<EditLayout />}></Route>
-          <Route path="/allSliders" element={<AllSliders />}></Route>
-          <Route path="/genreslist" element={<AllGenreList />}></Route>
-          <Route path="/LanguageList" element={<LanguageList />}></Route>{" "}
-          <Route
-            path="/QuestionUploader"
-            element={<EdducationQuestionGenerator />}
-          ></Route>{" "}
-          <Route path="allUsers" element={<AllUsers />}></Route>
-          <Route path="/userDetails/:uid" element={<UserDetails />}></Route>
-          
-          <Route path="*" element={<DashBoard />}></Route>
+          <Route path="*" element={<Login loggedIn={handleLogin}/>} />
         </Routes>
-      </div> */}
-      <Routes>
-        <Route path="/login" element={<Login/>} />
-      </Routes>
+      )}
     </div>
   );
 }
