efdbe8b80da3a4f7e1c08099b20428fc665c0856
Shivam Singh <ssultanpura9@gmail.com>
2025-01-28 18:29:30 +0530

    added all notification

--- src/components/Notification/AllNotification.js
+++ src/components/Notification/AllNotification.js
@@ -1,32 +1,118 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RoutesInfoDiv from "../RoutesInfoDiv";
 import { useSelector } from "react-redux";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const AllNotification = () => {
   const selectedTheme = useSelector((state) => state.theme.SelectedTheme);
   const [moreDetails, setMoreDetails] = useState(null);
-  const allNotifications = [
-    {
-      title: "my name is shivam",
-      status: "Pending",
-      description: "what are u doing is every functionality is working fine",
-      startTimeUnix: "123456000",
-      endTimeUnix: "56545333645",
-      repeat: "Minute",
-      lastSuccessMessage:
-        "notified successfully and added  Notification  for next schedule (IST …",
-      lastErrorMessage: "no error",
-      lastTaskLaunch: "25/1/2025, 6:03:00 pm",
-      nextTaskLaunch: "25/1/2025, 6:04:00 pm",
-    },
-  ];
+  const connectionString = process.env.REACT_APP_API_URL;
+  const [allNotifications, setAllNotifications] = useState([]);
+  useEffect(() => {
+    // getAllNotifications
+    async function fetchAllNotifications() {
+      try {
+        const res = await axios.get(
+          `${connectionString}/admin/getAllNotifications`,
+          {
+            headers: {
+              Authorization: localStorage.getItem("token"),
+            },
+          }
+        );
+
+        if (res?.data?.notificationTasks) {
+          setAllNotifications(res.data.notificationTasks);
+        }
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
+    }
+    fetchAllNotifications();
+  }, []);
+  const terminateJob = async (id) => {
+    try {
+      const res = await axios.get(
+        `${connectionString}/admin/terminateJob?taskId=${id}`,
+        {
+          headers: {
+            Authorization: localStorage.getItem("token"),
+          },
+        }
+      );
+      toast.success("job terminated successfully...");
+    } catch (error) {
+      console.log(error);
+      if (error?.response?.data?.msg) {
+        toast.error(error?.response?.data?.msg);
+      } else {
+        toast.error("something went wrong while terminating the job ");
+      }
+    }
+  };
+  // const allNotifications = [
+  //   {
+  //     title: "my name is shivam",
+  //     status: "Pending",
+  //     description: "what are u doing is every functionality is working fine",
+  //     startTimeUnix: "123456000",
+  //     endTimeUnix: "56545333645",
+  //     repeat: "Minute",
+  //     lastSuccessMessage:
+  //       "notified successfully and added  Notification  for next schedule (IST …",
+  //     lastErrorMessage: "no error",
+  //     lastTaskLaunch: "25/1/2025, 6:03:00 pm",
+  //     nextTaskLaunch: "25/1/2025, 6:04:00 pm",
+  //   },
+  //   {
+  //     title: "my name is shivam",
+  //     status: "Pending",
+  //     description: "what are u doing is every functionality is working fine",
+  //     startTimeUnix: "123456000",
+  //     endTimeUnix: "56545333645",
+  //     repeat: "Minute",
+  //     lastSuccessMessage:
+  //       "notified successfully and added  Notification  for next schedule (IST …",
+  //     lastErrorMessage: "no error",
+  //     lastTaskLaunch: "25/1/2025, 6:03:00 pm",
+  //     nextTaskLaunch: "25/1/2025, 6:04:00 pm",
+  //   },
+  //   {
+  //     title: "my name is shivam",
+  //     status: "Pending",
+  //     description: "what are u doing is every functionality is working fine",
+  //     startTimeUnix: "123456000",
+  //     endTimeUnix: "56545333645",
+  //     repeat: "Minute",
+  //     lastSuccessMessage:
+  //       "notified successfully and added  Notification  for next schedule (IST …",
+  //     lastErrorMessage: "no error",
+  //     lastTaskLaunch: "25/1/2025, 6:03:00 pm",
+  //     nextTaskLaunch: "25/1/2025, 6:04:00 pm",
+  //   },
+  //   {
+  //     title: "my name is shivam",
+  //     status: "Pending",
+  //     description: "what are u doing is every functionality is working fine",
+  //     startTimeUnix: "123456000",
+  //     endTimeUnix: "56545333645",
+  //     repeat: "Minute",
+  //     lastSuccessMessage:
+  //       "notified successfully and added  Notification  for next schedule (IST …",
+  //     lastErrorMessage: "no error",
+  //     lastTaskLaunch: "25/1/2025, 6:03:00 pm",
+  //     nextTaskLaunch: "25/1/2025, 6:04:00 pm",
+  //   },
+  // ];
   return (
     <div className=" w-[100%] h-[calc(100vh-70px)] overflow-y-scroll px-4 py-2">
       <RoutesInfoDiv
-        mainHeading={"All Movies"}
+        mainHeading={"All Notification"}
         websiteName={"Reelies"}
-        sectionName={"Movies section"}
-        currentDir={"All Movies"}
+        sectionName={"Notification section"}
+        currentDir={"All Notification"}
       ></RoutesInfoDiv>
       <section className="w-[100%]">
         {" "}
@@ -86,7 +172,7 @@ const AllNotification = () => {
               {allNotifications.length > 0 &&
                 allNotifications.map((current, index) => (
                   <>
-                    <div className="font-normal text-[.9rem] flex my-2  border-b border-gray-500">
+                    <div className="font-normal text-[.9rem] flex my-2  border-b border-gray-500 bg-gray-500 text-black items-center">
                       <div className="w-[50px] p-2  flex-shrink-0">
                         <p className="p-2">{index + 1}</p>
                       </div>
@@ -107,55 +193,70 @@ const AllNotification = () => {
                       <div className="w-[140px] flex-shrink-0">
                         {current.endTimeUnix}
                       </div>
-                      <div className="w-[80px]  flex-shrink-0">
-                       {moreDetails!=index? <p
-                          className="px-2 py-1 font-semibold bg-red-500 rounded-md text-white text-[.8rem] flex justify-center text-center cursor-pointer"
-                          onClick={() => {
-                            setMoreDetails(index);
-                          }}
-                        >
-                          View More
-                        </p>:<p
-                          className="px-2 py-1 font-semibold bg-red-500 rounded-md text-white text-[.8rem] flex justify-center text-center cursor-pointer"
-                          onClick={() => {
-                            setMoreDetails(null);
-                          }}
-                        >
-                          View Less
-                        </p>}
+                      <div className="w-[80px]  flex-shrink-0 px-1">
+                        {moreDetails != index ? (
+                          <p
+                            className="px-2 py-1 font-semibold bg-red-500 rounded-md text-white text-[.8rem] flex justify-center text-center cursor-pointer"
+                            onClick={() => {
+                              setMoreDetails(index);
+                            }}
+                          >
+                            View More
+                          </p>
+                        ) : (
+                          <p
+                            className="px-2 py-1 font-semibold bg-red-500 rounded-md text-white text-[.8rem] flex justify-center text-center cursor-pointer"
+                            onClick={() => {
+                              setMoreDetails(null);
+                            }}
+                          >
+                            View Less
+                          </p>
+                        )}
                       </div>
                     </div>
-                    {moreDetails==index&&<div className="grid grid-cols-2 2xl:grid-cols-3 gap-2 text-[.8rem]">
-                      <div className="flex">
-                        <p className="bg-yellow-600 h-fit p-1 rounded-md">
-                          Next Task Execution
-                        </p>
-                        <span className="px-2">{current.nextTaskLaunch}</span>
+                    {moreDetails == index && (
+                      <div className="grid grid-cols-2 2xl:grid-cols-3 gap-2 text-[.8rem]">
+                        <div className="flex">
+                          <p className="bg-yellow-600 h-fit p-1 rounded-md">
+                            Next Task Execution
+                          </p>
+                          <span className="px-2">{current.nextTaskLaunch}</span>
+                        </div>
+                        <div className="flex">
+                          <p className="bg-blue-600 h-fit p-1 rounded-md">
+                            Last Task Execution
+                          </p>
+                          <span className="px-2">{current.lastTaskLaunch}</span>
+                        </div>{" "}
+                        <div className="flex">
+                          <p className="bg-green-600 h-fit p-1 rounded-md ">
+                            Last Success Message
+                          </p>
+                          <span className="px-2">
+                            {current.lastSuccessMessage}
+                          </span>
+                        </div>
+                        <div className="flex">
+                          <p className="bg-red-600 h-fit p-1 rounded-md ">
+                            Last Error Message
+                          </p>
+                          <span className="px-2">
+                            {current.lastErrorMessage}
+                          </span>
+                        </div>{" "}
+                        {current.status !== "completed" && (
+                          <div
+                            className="bg-sky-600 border w-fit p-1 cursor-pointer"
+                            onClick={() => {
+                              terminateJob(current._id);
+                            }}
+                          >
+                            <p>Terminate Job</p>
+                          </div>
+                        )}
                       </div>
-                      <div className="flex">
-                        <p className="bg-blue-600 h-fit p-1 rounded-md">
-                          Last Task Execution
-                        </p>
-                        <span className="px-2">{current.lastTaskLaunch}</span>
-                      </div>{" "}
-                      <div className="flex">
-                        <p className="bg-green-600 h-fit p-1 rounded-md ">
-                          Last Success Message
-                        </p>
-                        <span className="px-2">
-                          {current.lastSuccessMessage}
-                        </span>
-                      </div>
-                      <div className="flex">
-                        <p className="bg-red-600 h-fit p-1 rounded-md ">
-                          Last Error Message
-                        </p>
-                        <span className="px-2">{current.lastErrorMessage}</span>
-                      </div>{" "}
-                      <div className="bg-sky-600 border w-fit p-1 cursor-pointer">
-                        <p>Terminate Job</p>
-                      </div>
-                    </div>}
+                    )}
                   </>
                 ))}
             </div>
